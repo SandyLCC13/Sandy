556b6bb4d0637aa7a3f1a1ada99dce86b541dd52
SandyLCC13 <133474070+SandyLCC13@users.noreply.github.com>
2024-05-03 19:15:40 -0600

    video

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -5,8 +5,7 @@ import css from './Video.module.css'
 const Video = props => {
     return( 
         <section >
-        
-    
+            <iframe src="https://drive.google.com/file/d/1e4mzw0-FyF2OQueNy3E_G4L5opCx2MGE/preview" width="640" height="480" allow="autoplay"></iframe>
         </section>
     )  
 }
